86fde7eb37a1947e01ed1785283b880fa01430b1
jsalazarSL <Jonathan.Salazar@starlightsoftwaresolutions.com>
2024-07-03 17:30:53 -0600

    removing useless things, and coloring round 1

---
+++ app/src/main/java/com/jsalazar/couplegame/GameBoardFragment.kt
@@ -0,0 +1,46 @@
+package com.jsalazar.couplegame
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.jsalazar.couplegame.databinding.FragmentGameBoardBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class GameBoardFragment : Fragment() {
+
+    private var _binding: FragmentGameBoardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentGameBoardBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            val popUpFragment = CardFragment()
+            popUpFragment.show((activity as AppCompatActivity).supportFragmentManager,context?.getString(R.string.app_name))
+            //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
